600b932213711c6da29c757b5d33f54b054f2eb1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-03 03:08:12 +0530

    sweet: overlay: Show "Turbo charging" instead of "Charging rapidly"
    * Like MIUI
    
    xawlw:
    Adapt to rro
    
    Change-Id: Ie3f916be54e65d76ae3be3df62bd6e54db280159

--- device.mk
+++ device.mk
@@ -112,6 +112,8 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlaySweet \
     DialerOverlaySweet \
     FrameworkResOverlaySweet \
+    SettingsOverlaySweet \
+    SettingsLibOverlaySweet \
     SettingsProviderOverlaySweet \
     SystemUIOverlaySweet \
     TelephonyOverlaySweet
