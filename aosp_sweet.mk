71b46244a0195613eca82c5d71b202e2f6b342d6
makhk <harikrish2050@gmail.com>
2021-07-25 10:52:13 +0530

    sweet: AEX bringups

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -5,15 +5,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common AEX configurations
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# Build with GApps if GAPPS_BUILD is true
+ifeq ($(GAPPS_BUILD),true)
+    WITH_GAPPS := true
+    TARGET_GAPPS_ARCH := arm64
+    IS_PHONE := true
+    TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+endif
 
-DEVICE_MAINTAINER := daniml3
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
