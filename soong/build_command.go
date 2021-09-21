ArrowOS-Devices/android_device_xiaomi_sweet

sweet: introduce a Soong module for running commands
* The sweet_build_command module allows to run commands, optionally
  after the modules under the `required` property are built

* The module can take the following properties:

- name (mandatory): the name of the target
- cmd (mandatory): the command in form of a string
- required (optional): a list of strings with the modules that need to
                       be built (and installed) before the command is
                       executed

* The command can contain variables that will be replaced by the
  corresponding value:

- $(productOut): will be replaced by the absolute path to the product's
  out (for example, /home/.../out/target/product/codename)
- $$: will be replaced by a normal $
- Other variables than the ones listed above will throw an error

Signed-off-by: daniml3 <danimoral1001@gmail.com>
Change-Id: I3d306684f8b295fdb83ef4429db1587402c1e1e1

soong/build_command.go
@@ -1,0 +1,93 @@
+package sweet_build_command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/google/blueprint"
+
+	"android/soong/android"
+)
+
+var (
+	moduleName = "sweet_build_command"
+	pctx       = android.NewPackageContext("android/soong/" + moduleName)
+)
+
+func init() {
+	android.InitRegistrationContext.RegisterModuleType(moduleName, GenRuleFactory)
+}
+
+type moduleProperties struct {
+	Cmd      string
+	Required []string
+}
+
+type Module struct {
+	android.ModuleBase
+	properties moduleProperties
+	outputPath android.WritablePath
+}
+
+func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	g.outputPath = android.PathForModuleGen(ctx)
+
+	if len(g.properties.Cmd) == 0 {
+		ctx.ModuleErrorf("%s", "missing property 'cmd'")
+		return
+	}
+
+	command, err := android.ExpandNinjaEscaped(g.properties.Cmd, func(name string) (string, bool, error) {
+		switch name {
+		case "productOut":
+			return ctx.Config().BuildDir() + "/../target/product/" + ctx.Config().DeviceName(), false, nil
+		default:
+			return "", false, fmt.Errorf("unknown variable '%s'", name)
+		}
+	})
+
+	if err == nil {
+		g.runCommand(ctx, command)
+	} else {
+		ctx.PropertyErrorf("cmd", "%s", err.Error())
+	}
+}
+
+func (g *Module) runCommand(ctx android.ModuleContext, command string) {
+	ruleParams := blueprint.RuleParams{
+		Command:     command,
+		CommandDeps: g.properties.Required,
+	}
+	rule := ctx.Rule(pctx, moduleName, ruleParams, nil...)
+	params := android.BuildParams{
+		Rule:   rule,
+		Output: g.outputPath,
+	}
+
+	ctx.Build(pctx, params)
+}
+
+func (g *Module) AndroidMk() android.AndroidMkData {
+	return android.AndroidMkData{
+		Include:    "$(BUILD_PHONY_PACKAGE)",
+		OutputFile: android.OptionalPathForPath(g.outputPath),
+		Extra: []android.AndroidMkExtraFunc{
+			func(w io.Writer, output android.Path) {
+				fmt.Fprintln(w, "LOCAL_ADDITIONAL_DEPENDENCIES := ", g.outputPath.String())
+			},
+		},
+	}
+}
+
+func NewGenRule() *Module {
+	module := &Module{}
+	module.AddProperties(&module.properties)
+
+	return module
+}
+
+func GenRuleFactory() android.Module {
+	m := NewGenRule()
+	android.InitAndroidModule(m)
+	return m
+}

makhk/device_xiaomi_sweet

soong/build_command.go
@@ -1,0 +1,88 @@
+package sweet_build_command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/google/blueprint"
+
+	"android/soong/android"
+)
+
+var (
+	moduleName = "sweet_build_command"
+	pctx       = android.NewPackageContext("android/soong/" + moduleName)
+)
+
+func init() {
+	android.InitRegistrationContext.RegisterModuleType(moduleName, SweetBuildCommandFactory)
+}
+
+type moduleProperties struct {
+	Cmd      string
+	Required []string
+}
+
+type Module struct {
+	android.ModuleBase
+	properties moduleProperties
+	outputPath android.WritablePath
+}
+
+func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	g.outputPath = android.PathForModuleGen(ctx)
+
+	if len(g.properties.Cmd) == 0 {
+		ctx.ModuleErrorf("%s", "missing property 'cmd'")
+		return
+	}
+
+	command, err := android.ExpandNinjaEscaped(g.properties.Cmd, func(name string) (string, bool, error) {
+		switch name {
+		case "productOut":
+			return ctx.Config().Getenv("OUT_DIR") + "/target/product/" + ctx.Config().DeviceName(), false, nil
+		default:
+			return "", false, fmt.Errorf("unknown variable '%s'", name)
+		}
+	})
+
+	if err == nil {
+		g.runCommand(ctx, command)
+	} else {
+		ctx.PropertyErrorf("cmd", "%s", err.Error())
+	}
+}
+
+func (g *Module) runCommand(ctx android.ModuleContext, command string) {
+	ruleParams := blueprint.RuleParams{
+		Command:     command,
+		CommandDeps: g.properties.Required,
+	}
+	rule := ctx.Rule(pctx, moduleName, ruleParams, nil...)
+	params := android.BuildParams{
+		Rule:        rule,
+		Description: " ",
+		Output:      g.outputPath,
+	}
+
+	ctx.Build(pctx, params)
+}
+
+func (g *Module) AndroidMk() android.AndroidMkData {
+	return android.AndroidMkData{
+		Include:    "$(BUILD_PHONY_PACKAGE)",
+		OutputFile: android.OptionalPathForPath(g.outputPath),
+		Extra: []android.AndroidMkExtraFunc{
+			func(w io.Writer, output android.Path) {
+				fmt.Fprintln(w, "LOCAL_ADDITIONAL_DEPENDENCIES := ", g.outputPath.String())
+			},
+		},
+	}
+}
+
+func SweetBuildCommandFactory() android.Module {
+	module := &Module{}
+	module.AddProperties(&module.properties)
+	android.InitAndroidModule(module)
+	return module
+}