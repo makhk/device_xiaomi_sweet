35b031cc5a1d6c1f6b4083681ba5954c35e2a47d
makhk <harikrish2050@gmail.com>
2021-12-24 18:24:51 +0530

    sweet: Go Arcana Official

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -5,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common AEX configurations
+# Inherit common Arcana configurations
 $(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_NAME := aosp_sweet
@@ -15,13 +15,12 @@ PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-# Build with GApps if GAPPS_BUILD is true
-ifeq ($(GAPPS_BUILD),true)
-    WITH_GAPPS := true
-    TARGET_GAPPS_ARCH := arm64
-    IS_PHONE := true
-    TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
-endif
+
+ARCANA_OFFICIAL := true
+ARCANA_DEVICE := sweet
+ARCANA_MAINTAINER := Hari(@makhk)
+TARGET_SUPPORTS_BLUR := true
+WITH_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
