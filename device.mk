a6a8fcc9ffafa2eae758b6f63bc8a07e6f65141d
kubersharma001 <kubersharma001@gmail.com>
2022-01-02 12:01:24 +0530

    sweet: Compile libldacBT_bco.so
    * E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found

--- device.mk
+++ device.mk
@@ -379,7 +379,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor
+    android.hardware.bluetooth@1.0.vendor \
+    libldacBT_bco
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
