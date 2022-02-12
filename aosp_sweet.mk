4a57be499944380f3705256d6fca7d052d03f5e7
makhk <harikrish2050@gmail.com>
2022-02-12 16:50:35 +0530

    sweet: Nuke build FP * ArcanaOS has it

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -22,10 +22,5 @@ ARCANA_MAINTAINER := Hari(@makhk)
 TARGET_SUPPORTS_BLUR := true
 WITH_GAPPS := true
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
-
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
